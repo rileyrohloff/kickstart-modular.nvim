a492599e545312b0e75b96ff58272f5a71af72e3
Riley <riley@CZM1003.local>
2025-08-14 10:53:59 -0600

    config: adding spell checking

--- init.lua
+++ init.lua
@@ -3,6 +3,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- spell check
+vim.opt.spell = true
+vim.opt.spelllang = 'en'
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
