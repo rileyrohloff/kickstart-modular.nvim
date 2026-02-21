71c07c950cdb8d31e4c5ce3b8cfb26e1980c88ef
Riley <riley@mac.lan>
2026-02-20 17:37:54 -0700

    feat: adding lazy plugins for gitlab and dart lang

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -28,6 +28,34 @@ require('lazy').setup({
     end,
   },
   {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
+  },
+  {
+    'https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim.git',
+    -- Activate when a file is created/opened
+    event = { 'BufReadPre', 'BufNewFile' },
+    -- Activate when a supported filetype is open
+    ft = { 'go', 'javascript', 'python', 'ruby' },
+    cond = function()
+      -- Only activate if token is present in environment variable.
+      -- Remove this line to use the interactive workflow.
+      return vim.env.GITLAB_TOKEN ~= nil and vim.env.GITLAB_TOKEN ~= ''
+    end,
+    opts = {
+      statusline = {
+        -- Hook into the built-in statusline to indicate the status
+        -- of the GitLab Duo Code Suggestions integration
+        enabled = true,
+      },
+    },
+  },
+  {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
     config = true,
